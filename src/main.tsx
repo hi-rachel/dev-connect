d1c9696837113aae350bfc7427e36634595c8a59
hi-rachel <rachel.uiux@gmail.com>
2024-04-01 09:46:09 +0900

    feat: react-query 설정, 개발자 도구

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
