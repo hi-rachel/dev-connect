a992c54a7c95ba98828c5b4b793611562ecf322b
hi-rachel <rachel.uiux@gmail.com>
2024-04-16 23:31:54 +0900

    feat: add tweets pagination(loadMore)

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,15 +1,18 @@
 import {
   collection,
+  getDocs,
   limit,
   onSnapshot,
   orderBy,
   query,
+  startAfter,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { db } from "../firebase";
 import Tweet from "./tweet";
 import { Unsubscribe } from "firebase/auth";
+import firebase from "firebase/compat/app";
 
 export interface ITweet {
   id: string;
@@ -29,20 +32,24 @@ const Wrapper = styled.div`
 `;
 
 export default function Timeline() {
-  const [tweets, setTweet] = useState<ITweet[]>([]);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [lastVisible, setLastVisible] =
+    useState<firebase.firestore.DocumentData | null>(null);
+
   useEffect(() => {
     let unsubscribe: Unsubscribe | null = null;
     const fetchTweets = () => {
       const tweetsQuery = query(
         collection(db, "tweets"),
         orderBy("createdAt", "desc"),
-        limit(50)
+        limit(20)
       );
       unsubscribe = onSnapshot(tweetsQuery, (snapshot) => {
-        const tweets = snapshot.docs.map((doc) => {
+        const newTweets: ITweet[] = [];
+        snapshot.forEach((doc) => {
           const { tweet, createdAt, userId, username, photo, userImg } =
             doc.data();
-          return {
+          newTweets.push({
             id: doc.id,
             tweet,
             createdAt,
@@ -50,21 +57,59 @@ export default function Timeline() {
             username,
             photo,
             userImg,
-          };
+          });
         });
-        setTweet(tweets);
+        setTweets(newTweets);
+        const lastVisibleDoc = snapshot.docs[snapshot.docs.length - 1];
+        setLastVisible(lastVisibleDoc);
       });
     };
+
     fetchTweets();
+
     return () => {
       unsubscribe && unsubscribe();
     };
   }, []);
+
+  const loadMore = async () => {
+    if (lastVisible) {
+      const tweetsRef = collection(db, "tweets");
+      const tweetsQuery = query(
+        tweetsRef,
+        orderBy("createdAt", "desc"),
+        startAfter(lastVisible),
+        limit(20)
+      );
+
+      const snapshot = await getDocs(tweetsQuery);
+      const newTweets: ITweet[] = [];
+      snapshot.forEach((doc) => {
+        const { tweet, createdAt, userId, username, photo, userImg } =
+          doc.data();
+        newTweets.push({
+          id: doc.id,
+          tweet,
+          createdAt,
+          userId,
+          username,
+          photo,
+          userImg,
+        });
+      });
+      setTweets((prevTweets) => [...prevTweets, ...newTweets]);
+
+      const lastVisibleDoc = snapshot.docs[snapshot.docs.length - 1];
+      setLastVisible(lastVisibleDoc);
+    }
+  };
+
   return (
     <Wrapper>
       {tweets.map((tweet) => (
         <Tweet key={tweet.id} {...tweet} />
       ))}
+      <button onClick={loadMore}>Load More</button>
     </Wrapper>
   );
 }
