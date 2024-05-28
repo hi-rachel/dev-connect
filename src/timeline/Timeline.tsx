8c7c68cea51412bb154248db003ba959d468c195
hi-rachel <rachel.uiux@gmail.com>
2024-05-29 01:26:16 +0900

    feat: update infinite scroll every 10 pages

--- src/timeline/Timeline.tsx
+++ src/timeline/Timeline.tsx
@@ -1,18 +1,17 @@
 import {
   collection,
-  // getDocs,
+  getDocs,
   limit,
   onSnapshot,
   orderBy,
   query,
-  // startAfter,
+  startAfter,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { db } from "../firebase";
 import Tweet from "./Tweet";
 import { Unsubscribe } from "firebase/auth";
-// import firebase from "firebase/compat/app";
 
 export interface ITweet {
   id: string;
@@ -32,28 +31,27 @@ const Wrapper = styled.div`
 `;
 
 const Loader = styled.div`
-  margin-top: 20px;
+  padding: 20px;
   text-align: center;
 `;
 
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
-  // const [lastVisible, setLastVisible] =
-  //   useState<firebase.firestore.DocumentData | null>(null);
-  // const [showLoadMore, setShowLoadMore] = useState(true);
-  const [loading, setLoading] = useState(false);
-  // const loaderRef = useRef<HTMLButtonElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMoreData, setHasMoreData] = useState(true);
+  const pageSize = 10;
 
   useEffect(() => {
     let unsubscribe: Unsubscribe | null = null;
-    const fetchTweets = () => {
-      setLoading(true);
+
+    const fetchTweets = async () => {
+      setIsLoading(true);
       const tweetsQuery = query(
         collection(db, "tweets"),
         orderBy("createdAt", "desc"),
-        limit(200)
+        limit(pageSize)
       );
-      unsubscribe = onSnapshot(tweetsQuery, (snapshot) => {
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
         const newTweets: ITweet[] = [];
         snapshot.forEach((doc) => {
           const { tweet, createdAt, userId, username, photo, userImg } =
@@ -68,87 +66,81 @@ export default function Timeline() {
             userImg,
           });
         });
-        setTweets((prevTweets) => [...prevTweets, ...newTweets]);
-        // const lastVisibleDoc = snapshot.docs[snapshot.docs.length - 1];
-        // setLastVisible(lastVisibleDoc);
-        // setShowLoadMore(snapshot.size >= 20);
-        setLoading(false);
+        setTweets((prevTweets) => [...prevTweets, ...newTweets]); // 기존 트윗 목록에 새로운 트윗 추가
+        setIsLoading(false);
       });
     };
 
     fetchTweets();
 
     return () => {
-      unsubscribe && unsubscribe();
+      if (unsubscribe) {
+        unsubscribe();
+      }
     };
   }, []);
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if (isBottom() && showLoadMore && !loading) {
-  //       loaderRef.current?.click();
-  //     }
-  //   };
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    if (
+      scrollTop + clientHeight + 100 >= scrollHeight &&
+      isLoading === false &&
+      hasMoreData === true
+    ) {
+      // 페이지 끝에 도달하면 추가 데이터를 받아온다
+      fetchNextTweets();
+    }
+  };
 
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [showLoadMore, loading]);
+  const fetchNextTweets = async () => {
+    console.log(tweets);
+    setIsLoading(true);
+    const lastTweet = tweets[tweets.length - 1];
+    const tweetsQuery = query(
+      collection(db, "tweets"),
+      orderBy("createdAt", "desc"),
+      startAfter(lastTweet?.createdAt),
+      limit(pageSize)
+    );
+    const snapshot = await getDocs(tweetsQuery);
+    const newTweets: ITweet[] = [];
+    snapshot.forEach((doc) => {
+      const { tweet, createdAt, userId, username, photo, userImg } = doc.data();
+      newTweets.push({
+        id: doc.id,
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        userImg,
+      });
+    });
+    setTweets((prevTweets) => [...prevTweets, ...newTweets]);
+    console.log(tweets);
+    setIsLoading(false);
+    if (snapshot.size < pageSize) {
+      // 페이지 당 데이터 크기보다 적게 받아왔으면 추가 데이터 없음을 표시
+      setHasMoreData(false);
+    }
+  };
 
-  // const isBottom = () => {
-  //   return (
-  //     window.innerHeight + window.scrollY >=
-  //     document.documentElement.scrollHeight
-  //   );
-  // };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-  // const loadMore = async () => {
-  //   if (lastVisible) {
-  //     setLoading(true);
-  //     const tweetsRef = collection(db, "tweets");
-  //     const tweetsQuery = query(
-  //       tweetsRef,
-  //       orderBy("createdAt", "desc"),
-  //       startAfter(lastVisible),
-  //       limit(20)
-  //     );
-
-  //     const snapshot = await getDocs(tweetsQuery);
-  //     const newTweets: ITweet[] = [];
-  //     snapshot.forEach((doc) => {
-  //       const { tweet, createdAt, userId, username, photo, userImg } =
-  //         doc.data();
-  //       newTweets.push({
-  //         id: doc.id,
-  //         tweet,
-  //         createdAt,
-  //         userId,
-  //         username,
-  //         photo,
-  //         userImg,
-  //       });
-  //     });
-  //     setTweets((prevTweets) => [...prevTweets, ...newTweets]);
-
-  //     const lastVisibleDoc = snapshot.docs[snapshot.docs.length - 1];
-  //     setLastVisible(lastVisibleDoc);
-  //     setShowLoadMore(snapshot.size >= 20);
-  //     setLoading(false);
-  //   }
-  // };
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
   return (
     <Wrapper>
-      {tweets.map((tweet) => (
-        <Tweet key={tweet.id} {...tweet} />
+      {tweets.map((tweet, index) => (
+        <Tweet key={`${tweet.id}-${index}`} {...tweet} />
       ))}
-      {loading && <Loader>Loading...</Loader>}
-      {/* {showLoadMore && (
-        <button ref={loaderRef} style={{ display: "none" }} onClick={loadMore}>
-          Load More
-        </button>
-      )} */}
+      {isLoading && <Loader>isLoading...</Loader>}
     </Wrapper>
   );
 }
