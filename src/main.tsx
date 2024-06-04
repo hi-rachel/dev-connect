b3538e7be274780b49f62af31a8b917850a7c881
hi-rachel <rachel.uiux@gmail.com>
2024-06-04 18:35:11 +0900

    uninstall: react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
